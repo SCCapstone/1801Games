abc6f200650272530ac106b182ab2397539023b8
Bradley Williamson <47425929+nulledBrad@users.noreply.github.com>
2020-10-22 20:40:41 -0400

    Add files via upload

---
+++ Score.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+//WRITTEN BY BRADLEY WILLIAMSON
+public class Score : MonoBehaviour
+{
+    public static Score instance;
+    public TextMeshProUGUI text;
+    private int score;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }    
+    }
+
+    // Update is called once per frame
+   public void ChangeScore(int coinValue)
+    {
+        score += coinValue;
+        text.text = "X" + score.ToString();
+    }
+
+    public int returnScore()
+    {
+        return score;
+    }
+}
+
