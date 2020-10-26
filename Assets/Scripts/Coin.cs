a42d41a9fc533fef863596532abfebbbf8304d63
nulledBrad <47425929+nulledBrad@users.noreply.github.com>
2020-10-26 05:30:37 -0400

    Credit For My Work
     needed to upload them for credit

--- Assets/Scripts/Coin.cs
+++ Assets/Scripts/Coin.cs
@@ -4,12 +4,15 @@ using UnityEngine;
 //WRITTEN BY BRADLEY WILLIAMSON
 public class Coin : MonoBehaviour
 {
+    // changes coinValue by 1
     public int coinValue = 1;
-
+    // the event that a trigger or a collison occurs
     private void OnTriggerEnter2D(Collider2D other)
     {
+        // if the colliding object is player
         if (other.gameObject.CompareTag("Player"))
         {
+            // update the score by 1
             Score.instance.ChangeScore(coinValue);
         }
     }
--- Assets/Scripts/Move2d.cs
+++ Assets/Scripts/Move2d.cs
@@ -7,14 +7,18 @@ using UnityEngine;
 // Classes Jump, OnTriggerEnter2D Written By Bradley Williamson
 public class Move2d : MonoBehaviour
 {
+    //player speed
     public float moveSpeed = 5f;
+    // bool check to see if player is on the ground
     public bool isGrounded = false;
+    // for speed boost
     public float boostTimer;
     public bool boost;
     public Animator animator;
     // Start is called before the first frame update
     void Start()
     {
+    // intiates boost and its timer
         boostTimer = 0;
         boost = false;
     }
@@ -22,12 +26,17 @@ public class Move2d : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+    //calls jump check
         Jump();
+        // player movement
         Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), 0f, 0f);
             transform.position += movement * Time.deltaTime * moveSpeed;
+            // speed boost check if boost is true
         if(boost)
         {
+        // add to boost timer
             boostTimer += Time.deltaTime;
+            // reset boost
             if(boostTimer >= 5)
             {
                 moveSpeed = 5f;
@@ -41,18 +50,22 @@ public class Move2d : MonoBehaviour
 
     void Jump()
     {
+    // if button down and player is grounded
         if (Input.GetButtonDown("Jump") && isGrounded == true) {
+        // jump
             gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(0f, 5f), ForceMode2D.Impulse);
                 }
         
     }
-
+    // collison events
     public void OnTriggerEnter2D(Collider2D other)
     {
+    // if collision with coin destroy coin
         if (other.gameObject.CompareTag("Coin"))
         {
             Destroy(other.gameObject);
         }
+        // if collison with gem destroy gem set move speed and boost true
         if (other.gameObject.CompareTag("Gem"))
         {
             Destroy(other.gameObject);
