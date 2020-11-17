480edd54a8512c08f668f99f91a9e2fd31373785
nulledBrad <47425929+nulledBrad@users.noreply.github.com>
2020-11-17 18:23:31 -0500

    PauseMenuUpdate
    -Added Pause Menu
    -Added Start Menu So you can Pause, Start, Quit
    -Linked Start - Pause - Settings - --Highscorres
    Added Scripts
    --PauseMenu
    --StartMenu

---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public static bool GamePaused = false;
+    public GameObject pauseMenuUI;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape)) {
+            if (GamePaused) {
+                Resume();
+            }
+            else {
+                Pause();
+            }
+        }
+        
+    }
+    void Pause() {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        GamePaused = true;
+    }
+    public void Resume() {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        GamePaused = false;
+    }
+
+    public void LoadMenu() {
+        SceneManager.LoadScene("StartMenu");
+    }
+    public void QuitMenu() {
+        Application.Quit();
+    }
+}
---
+++ Assets/Scripts/StartMenu.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartMenu : MonoBehaviour
+{
+    public GameObject StartMenuUI;
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void LoadGame() {
+        string MainScreen = "MAIN SCENE";
+        SceneManager.LoadScene(MainScreen);
+    }
+    public void LoadHighScores() {
+        string MainScreen = "highscores";
+        SceneManager.LoadScene(MainScreen);
+    }
+    public void LoadSettings() {
+        string MainScreen = "SettingsScreen";
+        SceneManager.LoadScene(MainScreen);
+    }
+    public void LoadQuit() {
+        Application.Quit();
+    }
+
+    
+}
