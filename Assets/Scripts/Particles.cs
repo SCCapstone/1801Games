bb37776269d6e0dd777bd8d953b89cb55c376d98
qiratt <tariqs@email.sc.edu>
2020-12-02 15:05:24 -0500

    Particle System Files
    Particle System for jumping

---
+++ Assets/Scripts/Particles.cs
@@ -0,0 +1,20 @@
+﻿// Written by Tariq Scott 
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Particles : MonoBehaviour
+{
+    [SerializeField] ParticleSystem collectParticle = null;
+
+    public void Update() {
+        if(Input.GetKeyDown(KeyCode.Space)) {
+            Collect();
+        }
+    }
+
+    public void Collect() {
+        collectParticle.Play();
+    }
+}
